762aedae470ec6cb8acf3bdd985cffe74dfcf5d0
Seventy <102102264+7Seventy0@users.noreply.github.com>
2022-04-11 08:17:50 +0200

    Source Code

---
+++ SeventysKunaiMod/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace SeventysKunaiMod
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.seventy.gorillatag.seventyskunaimod";
+        public const string Name = "SeventysKunaiMod";
+        public const string Version = "1.0.0";
+    }
+}
